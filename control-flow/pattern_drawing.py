ef6dfb9200f5d01a18cffa28092fa2127a7cc232
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-06-26 21:41:25 +0200

    pattern

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,9 @@
 length = int(input("Enter the size of the pattern: "))
+size = 0
 
-while length :
+while size < length:
     for i in range(length):
         print("*", end="")
     print()
+    size +=1
+print()
