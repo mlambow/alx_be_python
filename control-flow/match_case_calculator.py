368191519830227e04021ec70324d9a32575104f
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-06-26 11:23:30 +0200

    operation

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-operator = input("Choose the operator (+, -, *, /): ")
+operator = input("Choose the operation (+, -, *, /): ")
 
 match operator:
     case "+":
