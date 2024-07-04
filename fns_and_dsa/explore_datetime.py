73d7421340b0d876d65a65e2af984ab656dfe1ba
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-07-04 19:11:28 +0200

    datetime

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,13 +2,23 @@ from datetime import datetime, timedelta, date
 
 def display_current_datetime():
     current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%B-%d %H:%M:%S")
+    print(f"Current date and time: {formatted_date}")
 
-    print(current_date.strftime("%Y-%B-%d"))
-    print(current_date.strftime("%H:%M:%S"))
-display_current_datetime()
+def calculate_future_date(number_of_days ):
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=number_of_days )
+    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    print(f"Future date: {formatted_future_date}")
 
-def calculate_future_date():
-    number_of_days = int(input("Enter a number of days: "))
-    date = datetime.datetime.now() + timedelta(days=number_of_days)
-    future_date = date.strftime("%Y-%B-%d")
-calculate_future_date()
+def main():
+    display_current_datetime()
+
+    try:
+        number_of_days  = int(input("Enter a number of days: "))
+        calculate_future_date(number_of_days )
+    except ValueError:
+        print("Invalid input. Please enter an interger ")
+
+if __name__ == "__main__":
+    main()
