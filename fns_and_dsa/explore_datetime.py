86ee8a458a6d62fa54f7455f7d496a0ade12b69d
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-07-04 19:13:18 +0200

    datetime

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,13 +2,13 @@ from datetime import datetime, timedelta, date
 
 def display_current_datetime():
     current_date = datetime.now()
-    formatted_date = current_date.strftime("%Y-%B-%d %H:%M:%S")
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current date and time: {formatted_date}")
 
 def calculate_future_date(number_of_days ):
     current_date = datetime.now()
     future_date = current_date + timedelta(days=number_of_days )
-    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    formatted_future_date = future_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Future date: {formatted_future_date}")
 
 def main():
