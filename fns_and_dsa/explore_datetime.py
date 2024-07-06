ccc7e0868fd4a99e952d723674e8fa1de72df2af
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-07-06 16:39:54 +0200

    temperature

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,13 +1,13 @@
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
     formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current date and time: {formatted_date}")
 
-def calculate_future_date(number_of_days ):
+def calculate_future_date(number_of_days):
     current_date = datetime.now()
-    future_date = current_date + timedelta(days=number_of_days )
+    future_date = current_date + timedelta(days=number_of_days)
     formatted_future_date = future_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Future date: {formatted_future_date}")
 
@@ -16,9 +16,9 @@ def main():
 
     try:
         number_of_days  = int(input("Enter the number of days to add to the current date: "))
-        calculate_future_date(number_of_days )
+        calculate_future_date(number_of_days)
     except ValueError:
-        print("Invalid input. Please enter an interger ")
+        print("Invalid input. Please enter an interger")
 
 if __name__ == "__main__":
     main()
---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,11 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FSCTOR = 9/5
+temperature = float(input("What is your temperature?: "))
+weather = input("Is this weather in Celsuis or Fahrenheit?: ")
+
+def convert_to_celsuis(fahrenheit):
+    pass
+
+def convert_to_fehrenheit(celsuis):
+    pass
+
