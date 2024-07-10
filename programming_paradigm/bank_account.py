be2fb2c66e2ea30479c3e595594b1233de406203
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-07-10 23:27:41 +0200

    bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+        account_balance = 0
+
+    def deposit(self):
+        amount = float(input("Enter amount to be deposited: "))
+        self.account_balance += amount
+        print(f"Amount Deposited: ", amount)
+
+    def withdraw(self):
+        amount = float(input("Enter amount to withdrawn: "))
+        if self.account_balance >= amount:
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        print(f"Net Available balance = ", self.account_balance)
