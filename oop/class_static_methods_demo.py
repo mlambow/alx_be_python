0bd80b25d1817ddb2bf39cdb5d6a1ef70ea96686
Wandile Mlambo <wandile.mlambo@yahoo.com>
2024-07-21 19:48:48 +0200

    oop

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,11 @@
+class Calculator:
+    calculator_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+    
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculator_type}")
+        return a * b
